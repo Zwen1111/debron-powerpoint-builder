08048b098106fa3ab096d567a2cb7c8952fb37b9
Zwen van Erkelens <zwenvanerkelens@relatics.com>
2025-05-25 20:02:24 +0200

    Save songs in localstorage.

--- DeBron.PowerPoint.Builder/DeBron.PowerPoint.Builder/wwwroot/js/utils.js
+++ DeBron.PowerPoint.Builder/DeBron.PowerPoint.Builder/wwwroot/js/utils.js
@@ -26,3 +26,15 @@ window.registerPasteHandler = function (dotNetHelper, elementId) {
     });
 };
 
+window.localStorageFunctions = {
+    setItem: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    getItem: function (key) {
+        return localStorage.getItem(key);
+    },
+    removeItem: function (key) {
+        localStorage.removeItem(key);
+    }
+};
+
