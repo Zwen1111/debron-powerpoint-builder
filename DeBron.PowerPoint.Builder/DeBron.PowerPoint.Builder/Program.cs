d5dfa490e59446dd070d9ebc3c79b994576e31c4
Zwen van Erkelens <zwenvanerkelens@relatics.com>
2025-05-25 08:58:50 +0200

    Build image and publish to local registry.

--- DeBron.PowerPoint.Builder/DeBron.PowerPoint.Builder/Program.cs
+++ DeBron.PowerPoint.Builder/DeBron.PowerPoint.Builder/Program.cs
@@ -1,49 +1,57 @@
-using DeBron.PowerPoint.Builder;
 using DeBron.PowerPoint.Builder.Components;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace DeBron.PowerPoint.Builder;
 
-// Add services to the container.
-builder.Services.AddRazorPages(options => { options.RootDirectory = "/Components/Pages"; });
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddBlazorBootstrap();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options => { 
-        options.LoginPath = "/login";
-        options.AccessDeniedPath = "/login?error=true";
-        options.Cookie.SameSite = SameSiteMode.Lax;
-    });
-
-// Add logging to console
-builder.Logging.AddConsole();
-
-builder.Services.Configure<AuthenticationOptions>(builder.Configuration.GetSection("Authentication"));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages(options => { options.RootDirectory = "/Components/Pages"; });
+        builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+        builder.Services.AddBlazorBootstrap();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddAuthorization();
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.LoginPath = "/login";
+                options.AccessDeniedPath = "/login?error=true";
+                options.Cookie.SameSite = SameSiteMode.Lax;
+            });
+
+        // Add logging to console
+        builder.Logging.AddConsole();
+
+        builder.Services.Configure<AuthenticationOptions>(builder.Configuration.GetSection("Authentication"));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapRazorPages();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
---
+++ DeBron.PowerPoint.Builder/buildAndPush.ps1
@@ -0,0 +1,1 @@
+docker buildx build --platform linux/arm64 -t localhost:5000/debron-presentation-builder:latest --push .
