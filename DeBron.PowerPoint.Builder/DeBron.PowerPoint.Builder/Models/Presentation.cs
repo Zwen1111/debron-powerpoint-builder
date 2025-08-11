9c450410066e62b68536cf8642e4560d838eb329
Zwen van Erkelens <zwenvanerkelens@relatics.com>
2025-08-11 21:13:07 +0200

    Set theme.

---
+++ DeBron.PowerPoint.Builder/DeBron.PowerPoint.Builder/Models/Presentation.cs
@@ -0,0 +1,7 @@
+﻿namespace DeBron.PowerPoint.Builder.Models;
+
+public class Presentation
+{
+    public string Theme { get; set; } = "Dit is het thema";
+    public List<PresentationPart> Parts { get; init; } = new();
+}
